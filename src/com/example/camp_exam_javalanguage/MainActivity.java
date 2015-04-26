51ea32b1abb660929713ab1b8ec9f6069cdd80e9
mindwing <mindwing@gmail.com>
2015-04-27 02:13:53 +0900

    step_1_calc

---
+++ src/com/example/camp_exam_javalanguage/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.camp_exam_javalanguage;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	private TextView resultView;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_main);
+
+		resultView = (TextView) findViewById(R.id.result);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		String result = getCalcTest();
+		resultView.setText(result);
+	}
+
+	private String getCalcTest() {
+		String retVal = null;
+
+		int a = 5;
+		int b = 10;
+		
+		int y = a * 3 + b;
+
+		retVal = "a * 3 + b = " + Integer.toString(y);
+
+		return retVal;
+	}
+}
